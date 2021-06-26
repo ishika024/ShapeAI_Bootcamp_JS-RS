c706c397f138d0ce408ccd1cdb13da4927a30e44
Ishika Agarwal <86250466+ishika024@users.noreply.github.com>
2021-06-26 19:21:59 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>Full Stack Web Devlopment </h1>
+      <p>
+        It refers to the development of both front end and back end portions of
+        web application.
+      </p>
+      <h4>Tools:</h4>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>BOOTSTARP</li>
+        <li>JAVASCRIPT </li>
+        <li>REACTJS</li>
+        <li>NODEJS</li>
+        <li>MONGODB</li>
+      </ul>
+    </div>
+  );
+}
+export default Info;
